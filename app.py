90f5744f49e39d5107fcf6cf3c474361f440a43f
parkermerritt <parker.merritt@gmail.com>
2022-05-29 05:21:09 -0400

    modify homepage

--- app.py
+++ app.py
@@ -17,98 +17,86 @@ server = app.server
 
 app.scripts.config.serve_locally = False
 
-
 colorscale = cl.scales['9']['qual']['Paired']
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/dash-stock-ticker-demo.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/dash-stock-ticker-demo.csv')
 
 app.layout = html.Div([
+    
     html.Div([
-        html.H2('Finance Explorer',
+        html.H2('Mesh Methods',
                 style={'display': 'inline',
-                       'float': 'right',
+                       'float': 'left',
                        'font-size': '2.66em',
-                       'margin-left': '7px',
+                       'margin-left': '2em',
                        'font-weight': 'bolder',
                        'font-family': 'Product Sans',
                        'color': "rgba(117, 117, 117, 0.95)",
-                       'margin-top': '20px',
+                       'margin-top': '40px',
                        'margin-bottom': '0'
                        }),
         html.Img(src="./assets/MeshMethodsLogo-v1.png",
                 style={
-                    'height': '100px',
+                    'margin-right': '2em',
+                    'height': '120px',
                     'float': 'right'
                 },
         ),
     ]),
-    dcc.Dropdown(
-        id='stock-ticker-input',
-        options=[{'label': s[0], 'value': str(s[1])}
-                 for s in zip(df.Stock.unique(), df.Stock.unique())],
-        value=['YHOO', 'GOOGL'],
-        multi=True
-    ),
-    html.Div(id='graphs')
-], className="container")
+    
+])
 
-def bbands(price, window_size=10, num_of_std=5):
-    rolling_mean = price.rolling(window=window_size).mean()
-    rolling_std  = price.rolling(window=window_size).std()
-    upper_band = rolling_mean + (rolling_std*num_of_std)
-    lower_band = rolling_mean - (rolling_std*num_of_std)
-    return rolling_mean, upper_band, lower_band
 
-@app.callback(
-    dash.dependencies.Output('graphs','children'),
-    [dash.dependencies.Input('stock-ticker-input', 'value')])
-def update_graph(tickers):
-    graphs = []
 
-    if not tickers:
-        graphs.append(html.H3(
-            "Select a stock ticker.",
-            style={'marginTop': 20, 'marginBottom': 20}
-        ))
-    else:
-        for i, ticker in enumerate(tickers):
-
-            dff = df[df['Stock'] == ticker]
-
-            candlestick = {
-                'x': dff['Date'],
-                'open': dff['Open'],
-                'high': dff['High'],
-                'low': dff['Low'],
-                'close': dff['Close'],
-                'type': 'candlestick',
-                'name': ticker,
-                'legendgroup': ticker,
-                'increasing': {'line': {'color': colorscale[0]}},
-                'decreasing': {'line': {'color': colorscale[1]}}
-            }
-            bb_bands = bbands(dff.Close)
-            bollinger_traces = [{
-                'x': dff['Date'], 'y': y,
-                'type': 'scatter', 'mode': 'lines',
-                'line': {'width': 1, 'color': colorscale[(i*2) % len(colorscale)]},
-                'hoverinfo': 'none',
-                'legendgroup': ticker,
-                'showlegend': True if i == 0 else False,
-                'name': '{} - bollinger bands'.format(ticker)
-            } for i, y in enumerate(bb_bands)]
-            graphs.append(dcc.Graph(
-                id=ticker,
-                figure={
-                    'data': [candlestick] + bollinger_traces,
-                    'layout': {
-                        'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
-                        'legend': {'x': 0}
-                    }
-                }
-            ))
-
-    return graphs
+#@app.callback(
+    #dash.dependencies.Output('graphs','children'),
+    #[dash.dependencies.Input('stock-ticker-input', 'value')])
+#def update_graph(tickers):
+#    graphs = []
+#
+#    if not tickers:
+#        graphs.append(html.H3(
+#            "Select a stock ticker.",
+#            style={'marginTop': 20, 'marginBottom': 20}
+#        ))
+#    else:
+#        for i, ticker in enumerate(tickers):
+#
+#            dff = df[df['Stock'] == ticker]
+#
+#            candlestick = {
+#                'x': dff['Date'],
+#                'open': dff['Open'],
+#                'high': dff['High'],
+#                'low': dff['Low'],
+#                'close': dff['Close'],
+#                'type': 'candlestick',
+#                'name': ticker,
+#                'legendgroup': ticker,
+#                'increasing': {'line': {'color': colorscale[0]}},
+#                'decreasing': {'line': {'color': colorscale[1]}}
+#            }
+#            bb_bands = bbands(dff.Close)
+#            bollinger_traces = [{
+#                'x': dff['Date'], 'y': y,
+#                'type': 'scatter', 'mode': 'lines',
+#                'line': {'width': 1, 'color': colorscale[(i*2) % len(colorscale)]},
+#                'hoverinfo': 'none',
+#                'legendgroup': ticker,
+#                'showlegend': True if i == 0 else False,
+#                'name': '{} - bollinger bands'.format(ticker)
+#            } for i, y in enumerate(bb_bands)]
+#            graphs.append(dcc.Graph(
+#                id=ticker,
+#                figure={
+#                    'data': [candlestick] + bollinger_traces,
+#                    'layout': {
+#                        'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
+#                        'legend': {'x': 0}
+#                    }
+#                }
+#            ))
+#    return graphs
 
 
 if __name__ == '__main__':
